2a7ec9151fadea1da76bc8a76f0f6052698c06fa
azhmee21 <86728714+azhmee21@users.noreply.github.com>
2021-09-24 20:22:14 +0600

    Delete Sania directory

--- Sania/model/customertest.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include('customer.php');
-$customer=new customer();
-$customer->set_name('Arnob Rahee');
-echo $customer->get_name()."<br />";
-$customer->productsbought(2,"Indonesian Pearl");
-echo $customer->getproducts();
-?>
